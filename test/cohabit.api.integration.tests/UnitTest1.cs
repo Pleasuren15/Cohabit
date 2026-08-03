055118c763a07863cd01be06864ed53fd33fe468
Pleasure Ndhlovu <77783514+Pleasuren15@users.noreply.github.com>
2026-08-03 21:18:24 +0200

    wip: scaffolded project added (#30)

---
+++ test/cohabit.api.integration.tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace cohabit.api.integration.tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
